de6774dad90fd4d53298d855caedf543399345b4
skusVV <skus1992@gmail.com>
2018-12-05 09:13:27 +0200

    scroll settings and mobile view

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
+import {SmoothScroll} from '../../directives/scroll.directive';
 
 
 @Component({
@@ -7,13 +8,50 @@ import {Router} from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  scrollTo: string;
 
   constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd && this.router.url  === '/' && this.scrollTo) {
+          this.doScroll(this.scrollTo);
+        }
+      });
   }
 
   get isAdmin() {
     return this.router.url === '/admin';
   }
 
+  get isVacancies() {
+    return this.router.url === '/vacancies';
+  }
+
+  onClick(route, scrollTo) {
+    if (this.router.url  === '/vacancies' && route === '/') {
+      this.scrollTo = scrollTo;
+    }
+
+    if (this.router.url  === '/' && route === '/') {
+      this.doScroll(scrollTo);
+    }
+    this.router.navigate([route]);
+  }
+
+  private doScroll(scrollTo: string) {
+    setTimeout(() => {
+      new SmoothScroll(document.getElementById(scrollTo), {
+        duration: 800,
+        offset: null,
+        easing: null,
+        callbackBefore: () => {},
+        callbackAfter: () => {},
+        containerId: null
+      });
+    }, 200);
+  }
 }
