5ea3c541d03c046eeb52b9d725c5c3f86d9bf965
skusVV <skus1992@gmail.com>
2019-01-22 15:58:21 +0200

    made analityc works

--- src/services/analytic.service.ts
+++ src/services/analytic.service.ts
@@ -1,16 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnalyticService {
+  isBrowser: boolean
+
+  constructor(@Inject(PLATFORM_ID) private platformId) {
+    this.isBrowser = isPlatformBrowser(platformId);
+    if (this.isBrowser) {
+      this.nativeWindow.ga('create', 'UA-129165224-1', 'auto');
+    }
+  }
 
   get nativeWindow(): any {
     return window;
   }
 
   click(eventCategory: string, eventLabel: string, eventAction: string, eventValue: string) {
-    if (this.nativeWindow) {
+    if (this.isBrowser && this.nativeWindow) {
       this.nativeWindow.ga('send', 'event', eventCategory, eventAction, eventLabel, eventValue);
     }
   }
